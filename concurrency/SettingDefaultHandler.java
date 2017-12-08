d1d9f86bab42615e939c768e82d9c391e12aef59
ctuu <ct.liuu@gmail.com>
2017-12-08 15:38:26 +0800

    commit

---
+++ concurrency/SettingDefaultHandler.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.*;
+
+public class SettingDefaultHandler {
+  public static void main(String[] args) {
+    Thread.setDefaultUncaughtExceptionHandler(
+      new MyUncaughtExceptionHandler());
+    ExecutorService exec = Executors.newCachedThreadPool();
+    exec.execute(new ExceptionThread());
+  }
+}
